23f0b8503aa238fc53fea1fcda2ac96ff4299c25
David Hartman <david@petminded.com>
2015-05-30 16:57:37 -0700

    changed rockpaper...js using a simple || expression

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![General Assembly Logo](http://i.imgur.com/ke8USTq.png)
+q![General Assembly Logo](http://i.imgur.com/ke8USTq.png)
 
 # Rock Paper Scissors
 
--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,17 +22,22 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
+   var move = move|| getInput();
+
+
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return move;
 }
 
 function getComputerMove(move) {
+    var move = move || randomPlay();
+
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move;
 }
 
 function getWinner(playerMove,computerMove) {
