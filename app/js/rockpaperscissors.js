ff076d0b42b27fcc0bba2820eb536d60f2f7bcdd
David Hartman <david@petminded.com>
2015-06-21 12:19:40 -0700

    Final commit with end project PRE instructor review'

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -3,18 +3,18 @@
 ////////////////////////////////////////////////
 //'use strict';
 
-var playerWins = 0;
-var computerWins = 0;
-var storePlayerMove;
-var storeComputerMove;
-var rightFormat= ['rock', 'paper', 'scissors'];
+var playerWins = 0; //tracks the wins of the player
+var computerWins = 0; //tracks the wins of the mac
+var storePlayerMove; //pergame move of player
+var storeComputerMove; //pergame move of mac
+var rightFormat= ['rock', 'paper', 'scissors']; //proper input format to prevent cheaking
 
 
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.");
-    return prompt();
+    return prompt(); //request for choice from player
 }
-function randomPlay() {
+function randomPlay() { //use Math.random to generate mac's choice
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
         return "rock";
@@ -28,7 +28,7 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
-function getPlayerMove(move) {
+function getPlayerMove(move) { //to assure that the player has input a move
    var playerMove = move|| getInput();
     storePlayerMove = playerMove;
     return playerMove;
@@ -39,7 +39,7 @@ function getPlayerMove(move) {
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
 
-function getComputerMove(move) {
+function getComputerMove(move) { //calls to randomPlay to have a move generated for the mac
     var computerMove = move || randomPlay();
     storeComputerMove = computerMove;
     return computerMove;
@@ -49,16 +49,16 @@ function getComputerMove(move) {
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
 
-function getWinner(playerMove,computerMove) {
+function getWinner(playerMove,computerMove) { //if function that determins the winner of RPS
  var winner;
- var computerMove = getComputerMove();
- var playerMove = getPlayerMove();
+ computerMove = getComputerMove();
+ playerMove = getPlayerMove();
   // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     
-    if (-1 = rightFormat.indexOf(playerMove)) {
-        console.log("Human! Learn to read!" + '\n' + " You didn't use the right format. Use lower case letters ONLY" + '\n' +"This is declared a tie dispite your inferior brain!");
+    if (rightFormat.indexOf(playerMove) === -1) { //compairs the players input to the array rightFormat to assure format is compliant w code
+        console.log("Human! Learn to read!" + '\n' + " You didn't use the right format. Use lower case letters ONLY!" + '\n' +"This is declared a tie dispite your inferior brain!");
         winner= 'tie';
 
     }else if (computerMove == playerMove) {
@@ -80,19 +80,18 @@ function getWinner(playerMove,computerMove) {
 }
 
 
-function playToFive() {
+function playToFive() { //function calls other functions to action and loops till someone wins 5 games
     console.log("Let's play Rock, Paper, Scissors! ");
      while (playerWins < 5 && computerWins < 5){
              var whoWon = getWinner();    
         if (whoWon === 'computer') {
             computerWins +=1;
-            console.log( "You small human! You think you could keep up with me, 
-                the mighty computer? Computer's win: " +computerWins + '\n');
+            console.log( "You small human! You think you could keep up with me, the mighty computer? Computer's win: " +computerWins + '\n');
         } else if  (whoWon === 'player') {
             playerWins += 1;
             console.log( "Human you got lucky! Player's win: " + playerWins+ '\n');
         } else {
-            console.log( "Good Try human, tie. \n");
+            console.log( "A tie small human. \n");
         }
     }
     console.log ( 'Human played: ' + storePlayerMove +'.\n' +'Computer played: ' + storeComputerMove + '.\n' + 'WINNER IS: ' +whoWon + '!\n\n');
